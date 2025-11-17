6a3dd87d33a2c505211b6e6d7090548d1479a39e
Velichko Alexander <sasha0992@gmail.com>
2025-11-17 19:11:03 +0400

    fix: 4 task resolve

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -89,8 +89,9 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b <= c || Math.abs(a - b) >= c) return false;
+  return a === b || a === c || b === c;
 }
 
 /**
