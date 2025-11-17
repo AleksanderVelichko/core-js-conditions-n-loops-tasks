f4025aefc3580203fb29b07a1455a4866c14ef3c
Velichko Alexander <sasha0992@gmail.com>
2025-11-17 23:54:03 +0400

    fix: 15 task resolve

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -454,8 +454,36 @@ function shuffleChar(str, iterations) {
  * 321321   => 322113
  *
  */
-function getNearestBigger(/* number */) {
-  throw new Error('Not implemented');
+function getNearestBigger(number) {
+  const digits = [];
+  let arrNum = number;
+  while (arrNum > 0) {
+    digits.unshift(arrNum % 10);
+    arrNum = Math.floor(arrNum / 10);
+  }
+
+  let i = digits.length - 2;
+  while (i >= 0 && digits[i] >= digits[i + 1]) {
+    i -= 1;
+  }
+
+  if (i >= 0) {
+    let j = digits.length - 1;
+    while (digits[j] <= digits[i]) {
+      j -= 1;
+    }
+    [digits[i], digits[j]] = [digits[j], digits[i]];
+
+    const rightPart = digits.splice(i + 1);
+    digits.push(...rightPart.sort((a, b) => a - b));
+  }
+
+  let result = 0;
+  for (let k = 0; k < digits.length; k += 1) {
+    result = result * 10 + digits[k];
+  }
+
+  return result === number ? number : result;
 }
 
 module.exports = {
