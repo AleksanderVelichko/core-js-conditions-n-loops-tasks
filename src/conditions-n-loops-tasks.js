715ddc05f1939f260d678d2afb695d0998c50eed
Velichko Alexander <sasha0992@gmail.com>
2025-11-17 21:44:46 +0400

    fix: 9 task  resolve

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -240,8 +240,11 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  for (let n = num; n !== 0; n = Math.floor(n / 10)) {
+    if (n % 10 === digit) return true;
+  }
+  return false;
 }
 
 /**
